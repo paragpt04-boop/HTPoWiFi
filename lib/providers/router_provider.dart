63106748ec3bcdc3942fb72b5b6bbe68fb3cebae
paragpt04-boop <paragpt04@gmail.com>
2026-09-19 04:24:10 -0400

    fix: resolve RouterConfig name conflict in provider

--- lib/providers/router_provider.dart
+++ lib/providers/router_provider.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/router_config.dart';
 import '../services/mikrotik_api.dart';
